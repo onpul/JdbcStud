872a08a92664a2ffbcacd005280373ed7ea10491
onpul <chmj072@gmail.com>
2022-03-21 17:52:08 +0900

    20220321 JDBC 1일 차

--- JDBC01/src/com/test/Test001.java
+++ JDBC01/src/com/test/Test001.java
@@ -2,7 +2,7 @@ package com.test;
 
 public class Test001
 {
-	public static void main(String[] args) // main ctrl+space+enter
+	public static void main(String[] args) // main ctrl + space + enter
 	{
 		System.out.println("JAVA Test"); // syso + ctrl + space
 	}
