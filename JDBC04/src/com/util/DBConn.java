cd1eacd80ceda5f185168ad13ea4c499e8667e9c
onpul <chmj072@gmail.com>
2022-03-23 22:13:00 +0900

    20220323 DBConn 암기 완료~git add . !

--- JDBC04/src/com/util/DBConn.java
+++ JDBC04/src/com/util/DBConn.java
@@ -18,7 +18,9 @@ public class DBConn
 		if (dbConn == null)
 		{
 			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			
 			String user = "scott";
+			
 			String pwd = "tiger";
 			
 			Class.forName("oracle.jdbc.driver.OracleDriver");
@@ -32,27 +34,25 @@ public class DBConn
 	public static Connection getConnection(String url, String user, String pwd) throws ClassNotFoundException, SQLException
 	{
 		if (dbConn == null)
-		{	
+		{
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			
 			dbConn = DriverManager.getConnection(url, user, pwd);
 		}
 		
 		return dbConn;
 	}
 	
-	public static void close()
+	public static void close() throws SQLException 
 	{
 		if (dbConn != null)
 		{
 			if (!dbConn.isClosed())
 			{
 				dbConn.close();
-				
 			}
-			
 		}
 		
 		dbConn = null;
 	}
+	
 }
